f3e361a03132524be25f8db14edfc8d03f235d43
parsa jiravand <parsajiravand@gmail.com>
2022-05-06 19:21:11 +0430

    fix access controll

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ var PORT = process.env.PORT || 8080;
 const app = express()
 // Indicate to Express.js that you're using an additional plugin to treat parameters
 app.use(bodyParser.urlencoded({ extended: true }))
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // The code below starts the API with these parameters:
 // 1 - The PORT where your API will be available
