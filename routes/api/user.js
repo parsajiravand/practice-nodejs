96eb494f264e8a64f65b805a517cdd974dc560c9
parsa jiravand <parsajiravand@gmail.com>
2022-07-11 02:02:11 +0430

    add login API into swagger

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,19 +3,6 @@ const User = require("../../models/user");
 var passport = require('passport');
 
 // Replace the route name
-router.get("/", (request, response) => {
-  /* get All books  */
-  /* remove extra properties in find() */
-
-  User.find().exec((err, items) => {
-    if (!err) {
-      return response.json({ items: items });
-    } else {
-      return response.json(err);
-    }
-  });
-});
-
 router.post("/register", (request, response) => {
   const user = new User();
   user.email = request.body.email;
@@ -50,24 +37,4 @@ router.post('/login', function(req, res, next){
   })(req, res, next);
 });
 
-router.delete("/:id", (request, response) => {
-  Books.deleteOne({ _id: request.params.id })
-    .then(() => {
-      return response.json("deleted!");
-    })
-    .catch(function (error) {
-      console.log(error)
-       return response.json(error);
-    });
-});
-router.put("/:id", (request, response) => {
-  Books.updateOne({ _id: request.params.id },{ name: request.body.name })
-    .then(() => {
-      return response.json("updated!");
-    })
-    .catch(function (error) {
-      console.log(error)
-       return response.json(error);
-    });
-});
 module.exports = router;
